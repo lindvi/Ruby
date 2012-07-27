ab640fa1f74ffe9e0079b7a6bda9aeb5c7ac6a6b
Charlie Lindviken <lindvi@n167-p46.kthopen.kth.se>
2012-07-27 10:22:58 +0200

    added rpqs

--- db/migrate/20120723181026_add_index_to_rpq.rb
+++ db/migrate/20120723181026_add_index_to_rpq.rb
@@ -1,5 +1,11 @@
 class AddIndexToRpq < ActiveRecord::Migration
   def change
+	create_table :rpqs do |t|
+      t.integer :project_id
+      t.integer :question_id
+
+      t.timestamps
+    end
   	add_index :rpqs, [:project_id, :question_id], unique: true
   end
 end
