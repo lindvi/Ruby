65f9881942706e6f87ce61c0fcd0bff87fbee41f
Charlie Lindviken <lindvi@n167-p46.kthopen.kth.se>
2012-07-27 12:36:54 +0200

    Perty json on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Tlc::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #PERTY JSON! :D
+  config.action_view.pretty_print_json = false
 end
