6b7babfca086e41ce356dd686f798939e9cc0c77
Charlie Lindviken <lindvi@n167-p46.kthopen.kth.se>
2012-07-30 11:37:49 +0200

    fix

--- app/controllers/questionnaire_controller.rb
+++ app/controllers/questionnaire_controller.rb
@@ -1,8 +1,5 @@
 class QuestionnaireController < ApplicationController
-
 	def show
 		@question = Question.find(params[:id])
-		@answer = Answer.new(user_id: current_user.id, question_id: @question.id, remember_token: current_user.remember_token)
 	end
-
 end
